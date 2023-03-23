bd7b13a7f056d911adde9fe22debad79c16070c4
Pelz86 <c.bola86@yahoo.com>
2023-03-23 14:32:57 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,23 @@
 
 void print_triangle(int size)
 {
+	int row, hashes, spaces;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
-		{
-			for (j = i; j < size; j++)
+		for (row = 1; row <= size; row++)
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('%');
-			}
-			_putchar('\n');
+		for (hashes = 1; hashes <= row; hashes++)
+		{
+			_putchar('#');
 		}
 	}
 }
+

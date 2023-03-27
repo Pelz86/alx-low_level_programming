3cd8707e5d8ba2bbd35277b24ba694e6d82c14ec
Pelz86 <c.bola86@yahoo.com>
2023-03-27 14:51:40 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,12 +19,12 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';

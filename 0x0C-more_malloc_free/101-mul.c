f1379c3fea635b04d98cbb9e20e406ed8fc33886
Pelz86 <c.bola86@yahoo.com>
2023-04-13 15:09:48 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -39,15 +39,16 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
 /**
  * errors - handles errors for main
  */
-
 void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -61,13 +62,12 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-		result = malloc(sizeof(int) * len);
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
@@ -82,7 +82,6 @@ int main(int argc, char *argv[])
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
